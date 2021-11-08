79e8815448763e0e83615c3a5ef9fd47e466b80f
slhscs319 <burrito246897531@gmail.com>
2021-11-08 13:42:18 -0600

    add player class

---
+++ Project/src/Player.java
@@ -0,0 +1,24 @@
+
+
+public class Player {
+
+    AdventurerCard adventurerCard;
+    Pawn pawn;
+    ArrayList<TreasureCard> treasureCardHand = new ArrayList<TreasureCard>();
+
+    //constructs new Player given String parameter adventurer
+    public Player(String adventurer) {
+        adventurerCard = new AdventurerCard(adventurer);
+        pawn = new Pawn(adventurer);
+    }
+
+    //moves player Pawn to given tile coordinates and returns true if move was successful
+    public boolean move(int row, int col) {
+        if (if adventurerCard==)
+    }
+
+    public boolean shoreUp() {}
+
+
+
+}
