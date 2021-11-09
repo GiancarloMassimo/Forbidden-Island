866c5310390a14f6ce9aed15e5db17d82ad43e3a
manlol33633 <kraven.clark789456123@gmail.com>
2021-11-09 13:37:36 -0600

    Main Panel

--- Project/src/MainMenu.java
+++ Project/src/MainMenu.java
@@ -1,13 +1,38 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
+import java.util.Random.*;
 
-public class MainMenu extends JFrame {
-    private static final int WIDTH = 1600;
-    private static final int HEIGHT = 900;
+public class MainMenu extends JFrame implements ItemListener, ActionListener {
+    private Container win;
+
+    private JLabel screenLabel, inputLabel, outputLabel, inputBaseLabel;
+    private JTextField inputField;
+    private JScrollPane scrollPane;
+    private JTextArea outputArea;
+    private JCheckBox binCheck, hexCheck, decCheck;
+    private JButton printButton;
+    private JRadioButton dec_irbutton, bin_irbutton, hex_irbutton;
+    private ButtonGroup  input_radioGroup;
+
+    private static final int WIDTH = 936;
+    private static final int HEIGHT = 557;
     public MainMenu(String framename){
         super(framename);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(WIDTH, HEIGHT);
+        setResizable(false);
+        add(new MainPanel());
         setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+
     }
 }
