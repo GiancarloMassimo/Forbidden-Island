76da449e8e084522e64537b187006b2ce3a8df80
shawnlinhca <71190839+shawnlinhca@users.noreply.github.com>
2021-11-11 13:30:36 -0600

    deck display

--- Project/src/MainMenu.java
+++ Project/src/MainMenu.java
@@ -5,16 +5,16 @@ import java.awt.event.ActionEvent;
 import java.util.Random.*;
 import java.awt.FlowLayout;
 
+
 public class MainMenu extends JFrame implements ActionListener {
-    private static final int WIDTH = 936;
-    private static final int HEIGHT = 557;
+    private static final int WIDTH = 1600;
+    private static final int HEIGHT = 960;
+
     public MainMenu(String framename){
         super(framename);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(WIDTH, HEIGHT);
-        JButton btn = new JButton("Click Here");
-        btn.setBounds(50, 100, 95, 30);
-        add(btn);
+
         setResizable(false);
         setLayout(null);
         add(new MainPanel());
