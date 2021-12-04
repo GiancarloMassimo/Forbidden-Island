98d3b3ed47f433483e46541ad15c8fcf0483f312
burri <burrito246897531@gmail.com>
2021-12-03 22:02:42 -0600

    Finished pilot class

--- Project/src/PlayerPilot.java
+++ Project/src/PlayerPilot.java
@@ -1,9 +1,15 @@
 public class PlayerPilot extends Player{
 
+    //Since pilot can only use special move once per turn, he has to have boolean which turns false after using in a turn
     private boolean abilityIsAvailable = false;
+
+    public void startTurn() {
+        abilityIsAvailable = true;
+    }
 
     public void moveSpecial(int row, int col) {
         super.move(row, col);
+        abilityIsAvailable = false;
     }
 
     public boolean canMoveSpecial(int row, int col) {
