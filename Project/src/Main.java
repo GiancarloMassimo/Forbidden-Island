8ae2bc41fa2ce46b3d6fddeead599564e24483e8
GiancarloMassimo <gmassimo2004@gmail.com>
2021-12-05 06:03:22 -0600

    Bonus points

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,27 +1,47 @@
+import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import static java.lang.System.*;
 
 public class Main {
     public static FloodDeck floodDeck;
     static TreasureDeck treasureDeck;
-    public static ArrayList<Player> players = new ArrayList<>();
+    public static ArrayList<Player> players = new ArrayList<>(), playerPool = new ArrayList<>();
     public static Player currentPlayer;
 
     public static void main(String[] args) {
         out.println("Game Seed: " + RandomUtil.seed);
         Map map = new Map();
-        Player player1 = new PlayerNavigator();
-        Player player2 = new PlayerNavigator();
-        Player player3 = new PlayerNavigator();
-        Player player4 = new PlayerNavigator();
 
-        currentPlayer = player1;
+        playerPool.add(new PlayerNavigator());
+        playerPool.add(new PlayerDiver());
+        playerPool.add(new PlayerEngineer());
+        playerPool.add(new PlayerPilot());
+        playerPool.add(new PlayerExplorer());
+        playerPool.add(new PlayerMessenger());
 
-        players.add(player1);
-        players.add(player2);
-        players.add(player3);
-        players.add(player4);
+        Collections.shuffle(playerPool);
+
+        int playerCount = 4;
+        try { playerCount = Integer.parseInt(JOptionPane.showInputDialog("Enter the number of players (2 - 4)"));  } catch (Exception e) { playerCount = 4; }
+
+        if (playerCount < 2) playerCount = 2; if (playerCount > 4) playerCount = 4;
+
+        int startingWater = 1;
+        try { startingWater = Integer.parseInt(JOptionPane.showInputDialog("Enter the startingWaterLevel (1 - 4)")); } catch (Exception e) { startingWater = 1; }
+
+        if (startingWater < 1) startingWater = 1; if (startingWater > 4) startingWater = 4;
+
+        WaterLevel.waterMarkerLevel = startingWater;
+        if (startingWater >= 3) {
+            WaterLevel.waterLevel = 3;
+        }
+
+
+        for (int i = 0; i < playerCount; i++) {
+            players.add(playerPool.remove(0));
+        }
 
         MainMenuFrame graphics = new MainMenuFrame("Main Menu");
         floodDeck = new FloodDeck();
